9a67e6611c8592c7efdedefdd4d10d66e69d1cdd
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-07-06 12:05:49 +0900

    1. cp from ani-interaction

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function App() {
+  return <Container></Container>;
+}
